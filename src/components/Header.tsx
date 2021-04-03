18b751f93565c2a1159c398191e25ae24e800402
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-04-03 10:38:10 -0300

    Criando Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,43 @@
+import { Flex, Text, Input, Icon } from '@chakra-ui/react'
+import { RiSearchLine } from 'react-icons/ri'
+
+export function Header() {
+    return (
+        <Flex
+            as='header'
+            w='100%'
+            maxWidth={1480}
+            h='20'
+            mx='auto'
+            mt='4'
+            px='6'
+            align='center'>
+            <Text
+                fontSize='3xl'
+                fontWeight='bold'
+                letterSpacing='tight'
+                w='64'>
+                DashGo
+                <Text as='span' color='pink.500' ml='1'>.</Text>
+            </Text>
+
+            <Flex
+                as='label'
+                flex='1'
+                py='4'
+                px='8'
+                ml='6'
+                maxWidth={400}
+                align='center'
+                color='gray.200'
+                position='relative'
+                bg='gray.800'
+                borderRadius='full'
+            >
+                <Input color='gray.500' variant='unstyled' placeholder='Buscar na plataforma'
+                    _placeholder={{ color: 'gray.400' }} mr='4' />
+                <Icon as={RiSearchLine} fontSize='24' />
+            </Flex>
+        </Flex>
+    )
+}
