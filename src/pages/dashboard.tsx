15d867f008a0e5456921087aa991dbaf20bba0b7
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-04-03 11:21:15 -0300

    Componente: Sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,13 @@
 import { Header } from "../components/Header";
-
+import { Flex } from '@chakra-ui/react'
+import { Sidebar } from "../components/Siderbar";
 export default function Dashboard() {
     return (
-        <Header />
+        <Flex direction='column' h='100vh'>
+            <Header />
+            <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
+                <Sidebar />
+            </Flex>
+        </Flex>
     )
 }
