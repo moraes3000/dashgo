405e6126b4ce6afda9c809304b659ae37ab505ee
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-04-14 19:55:46 -0300

    Configurando React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,23 +1,30 @@
-import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Tbody, Td, Text, Checkbox, useBreakpointValue } from "@chakra-ui/react"
+import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Tbody, Td, Text, Checkbox, useBreakpointValue, Spinner } from "@chakra-ui/react"
 import Link from "next/link"
 import { useEffect } from "react"
 import { RiAddLine, RiPencilLine } from "react-icons/ri"
+
+import { useQuery } from 'react-query'
 
 import { Header } from "../../components/Header"
 import { Pagination } from "../../components/Pagination"
 import { Sidebar } from "../../components/Sidebar"
 
 export default function UserList() {
+
+    const { data, isLoading, error } = useQuery('users', async () => {
+        const response = await fetch('http://localhost:3000/api/users')
+        const data = await response.json()
+
+        return data
+    })
+    console.log(data)
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
     })
 
-    useEffect(() => {
-        fetch('http://localhost:3000/api/users')
-            .then(response => response.json())
-            .then(data => console.log(data))
-    }, [])
+
 
     return (
         <Box>
@@ -26,111 +33,124 @@ export default function UserList() {
             <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
                 <Sidebar />
                 <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
-                    <Flex mb='8' justify='space-between' align='center'>
-                        <Heading size='lg' fontWeight='normal'>
-                            Usuários
-                        </Heading>
-                        <Link href='/users/create' passHref>
-                            <Button
-                                as='a'
-                                size='sm'
-                                fontSize='sm' colorScheme='pink'
-                                leftIcon={<Icon as={RiAddLine} fontSize={20} />}>
-                                Criar usuário
-                        </Button>
-                        </Link>
-                    </Flex>
 
-                    <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
-                        <Flex mb='8' justify='space-between' align='center'>
-                            <Table colorScheme='whiteAlpha'>
-                                <Thead>
-                                    <Tr>
-                                        <Th px={['4', '4', '6']} color='gray.300' width='8'>
-                                            <Checkbox colorScheme='pink' />
-                                        </Th>
-                                        <Th>
-                                            Usuário
-                                        </Th>
-                                        {isWideVersion && <Th>  Data de cadastro </Th>}
-
-
-                                        <Th width='8'></Th>
-                                    </Tr>
-                                </Thead>
-
-                                <Tbody>
-                                    <Tr>
-                                        <Td px={['4', '4', '6']}>
-                                            <Checkbox colorScheme='pink' />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontSize='bold'>Bruno Moraes</Text>
-                                                <Text fontSize='sm' color='gray.300'>bruno_bmoraes@hotmail.com</Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion && <Td>03 de Abril,2021</Td>}
-                                        <Td>
-                                            <Button
-                                                as='a'
-                                                size='sm'
-                                                fontSize='sm' colorScheme='purple'
-                                                leftIcon={<Icon as={RiPencilLine} fontSize={16} />}>
-                                                Editar
-                                            </Button>
-                                        </Td>
-                                    </Tr>
-
-                                    <Tr>
-                                        <Td px={['4', '4', '6']}>
-                                            <Checkbox colorScheme='pink' />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontSize='bold'>Bruno Moraes</Text>
-                                                <Text fontSize='sm' color='gray.300'>bruno_bmoraes@hotmail.com</Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion && <Td>03 de Abril,2021</Td>}
-                                        <Td>
-                                            <Button
-                                                as='a'
-                                                size='sm'
-                                                fontSize='sm' colorScheme='purple'
-                                                leftIcon={<Icon as={RiPencilLine} fontSize={16} />}>
-                                                Editar
-                                            </Button>
-                                        </Td>
-                                    </Tr>
-
-                                    <Tr>
-                                        <Td px={['4', '4', '6']}>
-                                            <Checkbox colorScheme='pink' />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontSize='bold'>Bruno Moraes</Text>
-                                                <Text fontSize='sm' color='gray.300'>bruno_bmoraes@hotmail.com</Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion && <Td>03 de Abril,2021</Td>}
-                                        <Td>
-                                            <Button
-                                                as='a'
-                                                size='sm'
-                                                fontSize='sm' colorScheme='purple'
-                                                leftIcon={<Icon as={RiPencilLine} fontSize={16} />}>
-                                                Editar
-                                            </Button>
-                                        </Td>
-                                    </Tr>
-                                </Tbody>
-                            </Table>
+                    {isLoading ? (
+                        <Flex justify='center'>
+                            <Spinner />
                         </Flex>
+                    ) : error ? (
+                        <Flex justify='center'>
+                            <Text>Falha ao obter os dados</Text>
+                        </Flex>
+                    ) : (
+                        <>
+                            <Flex mb='8' justify='space-between' align='center'>
+                                <Heading size='lg' fontWeight='normal'>
+                                    Usuários
+                        </Heading>
+                                <Link href='/users/create' passHref>
+                                    <Button
+                                        as='a'
+                                        size='sm'
+                                        fontSize='sm' colorScheme='pink'
+                                        leftIcon={<Icon as={RiAddLine} fontSize={20} />}>
+                                        Criar usuário
+                        </Button>
+                                </Link>
+                            </Flex>
 
-                        <Pagination />
-                    </Box>
+                            <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
+                                <Flex mb='8' justify='space-between' align='center'>
+                                    <Table colorScheme='whiteAlpha'>
+                                        <Thead>
+                                            <Tr>
+                                                <Th px={['4', '4', '6']} color='gray.300' width='8'>
+                                                    <Checkbox colorScheme='pink' />
+                                                </Th>
+                                                <Th>
+                                                    Usuário
+                                        </Th>
+                                                {isWideVersion && <Th>  Data de cadastro </Th>}
+
+
+                                                <Th width='8'></Th>
+                                            </Tr>
+                                        </Thead>
+
+                                        <Tbody>
+                                            <Tr>
+                                                <Td px={['4', '4', '6']}>
+                                                    <Checkbox colorScheme='pink' />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontSize='bold'>Bruno Moraes</Text>
+                                                        <Text fontSize='sm' color='gray.300'>bruno_bmoraes@hotmail.com</Text>
+                                                    </Box>
+                                                </Td>
+                                                {isWideVersion && <Td>03 de Abril,2021</Td>}
+                                                <Td>
+                                                    <Button
+                                                        as='a'
+                                                        size='sm'
+                                                        fontSize='sm' colorScheme='purple'
+                                                        leftIcon={<Icon as={RiPencilLine} fontSize={16} />}>
+                                                        Editar
+                                            </Button>
+                                                </Td>
+                                            </Tr>
+
+                                            <Tr>
+                                                <Td px={['4', '4', '6']}>
+                                                    <Checkbox colorScheme='pink' />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontSize='bold'>Bruno Moraes</Text>
+                                                        <Text fontSize='sm' color='gray.300'>bruno_bmoraes@hotmail.com</Text>
+                                                    </Box>
+                                                </Td>
+                                                {isWideVersion && <Td>03 de Abril,2021</Td>}
+                                                <Td>
+                                                    <Button
+                                                        as='a'
+                                                        size='sm'
+                                                        fontSize='sm' colorScheme='purple'
+                                                        leftIcon={<Icon as={RiPencilLine} fontSize={16} />}>
+                                                        Editar
+                                            </Button>
+                                                </Td>
+                                            </Tr>
+
+                                            <Tr>
+                                                <Td px={['4', '4', '6']}>
+                                                    <Checkbox colorScheme='pink' />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontSize='bold'>Bruno Moraes</Text>
+                                                        <Text fontSize='sm' color='gray.300'>bruno_bmoraes@hotmail.com</Text>
+                                                    </Box>
+                                                </Td>
+                                                {isWideVersion && <Td>03 de Abril,2021</Td>}
+                                                <Td>
+                                                    <Button
+                                                        as='a'
+                                                        size='sm'
+                                                        fontSize='sm' colorScheme='purple'
+                                                        leftIcon={<Icon as={RiPencilLine} fontSize={16} />}>
+                                                        Editar
+                                            </Button>
+                                                </Td>
+                                            </Tr>
+                                        </Tbody>
+                                    </Table>
+                                </Flex>
+
+                                <Pagination />
+                            </Box>
+                        </>
+                    )}
                 </Box>
             </Flex>
         </Box>
